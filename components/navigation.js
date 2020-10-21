e318080b384439cbb3430f12b17cb3e085093a90
John Bryan Delda <mightypirates174@gmail.com>
2020-10-21 13:21:36 +0800

    Navigation - reworked client navigation and UI design registerForm - improve event handling

--- components/navigation.js
+++ components/navigation.js
@@ -1,17 +1,46 @@
-import Link from 'next/link'
-import navStyle from '../styles/index.module.sass'
-export default function Navigation(){
-return(
-    <React.Fragment >
-        <div className={navStyle.sidebar}>
-                <ul>
-                    <li><Link href={`/`}>Dashboard</Link></li>
-                    <li><Link href={`/vehicle-register`}>Register</Link></li>
-                    <li><Link href={`/data-registry`}>Registry</Link></li>
-                    <li><Link href={`/permission`}>Permission</Link></li>
-                    <li><Link href={`/settings`}>Settings</Link></li>
-                </ul>
-                </div>
-    </React.Fragment>   
-)
+import Link from "next/link";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Nav from "react-bootstrap/Nav";
+export default function Navigation() {
+  return (
+    <React.Fragment>
+      <Row className="d-flex justify-content-center align-items-center my-3 flex-column">
+        <Image src="/profile.jpg" width="120px" height="120px" roundedCircle />
+        <h4 className="mt-3 lead">John Bryan Delda</h4>
+        <p>
+          <small>Admin</small>
+        </p>
+      </Row>
+      <Row className="d-flex justify-content-center">
+        <Nav defaultActiveKey="/" variant="pills" className="flex-column">
+          <Nav.Item>
+            <Nav.Link>
+              <Link href={`/`}>Home</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link href={`/vehicle-register`}>Vehicles</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link href={`/data-registry`}>Registry</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link href={`/permission`}>Permission</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link href={`/settings`}>Settings </Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Row>
+    </React.Fragment>
+  );
 }
--- components/permission/registerForm.js
+++ components/permission/registerForm.js
@@ -2,19 +2,85 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+
+const evalPasswordLength =(password)=>{
+      let length = password.length
+      if( length >= 0 && length < 8 ) {
+        return {
+        helpText: 'Your password is less than the minimum(8) required characters .',
+        textStyle: "text-danger"
+      }}
+      if(length >= 8) {
+        return {
+        helpText: 'Your password is greater than the minimum(8) required characters.',
+        textStyle: "text-success"
+      }}
+    }
+
 export default function RegisterForm() {
-  var [disableRoles, setDisableRoles] = useState({
-    preRoles: false,
-    customRoles: true,
-  });
-  const handleCustomeRole = (e) => {
-    if (e.currentTarget.checked || !e.currentTarget.checked) {
-      setDisableRoles({
-        preRoles: !disableRoles.preRoles,
-        customRoles: !disableRoles.customRoles,
-      });
+  var [passwordHelpText, setPasswordHelpText] = useState({
+    text:'Password must be at least 8 characters long.',
+    style: "text-muted"
+  })
+  var [formData, setFormData] = useState({
+    firstName:'',
+    lastName:'',
+    userName: '',
+    password: '',
+  })
+  var [formRole,setFormRole]= useState({
+    enabled:true,
+    value:"",
+  })
+  var [customPermissions, setCustomPermissions]= useState({
+    enabled: false,
+    permissions:[]
+  })
+  var [enableCustomRole, setCustomRoleDisable] = useState(false)
+  
+  
+  const handleCustomRoleEnabler = (e) => {
+    if(e.currentTarget.checked) {
+      setCustomPermissions({
+      enabled: true,
+      permissions:[]
+      }) 
+      setFormRole({
+        enabled:false
+      })
+    } 
+    if(!e.currentTarget.checked) {
+      setCustomPermissions ({
+        enabled:false
+      })
+      setFormRole({
+        enabled:true
+      })
     }
   };
+  const handleFormRoleSelection =(e)=> {
+    setFormRole({
+      value: e.currentTarget.value
+    })
+  }
+  const handlePasswordInput=(inputPassword)=>{  
+    let {helpText, textStyle} = evalPasswordLength(inputPassword)
+    setFormData({
+      password: inputPassword
+    })
+    setPasswordHelpText({
+      text: helpText,
+      style: textStyle
+    })
+  }
+
+  const handleAddPermission=(e)=>{
+    let permission = e.currentTarget.value
+    setCustomRole({
+      permissions: [...customRole.permissions,permission]
+    })
+  }
+  
   return (
     <React.Fragment>
       <Form className="p-3">
@@ -28,21 +94,21 @@ export default function RegisterForm() {
           <Col>
             <Form.Group controlId="formGridFirstName">
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter First Name" />
+              <Form.Control type="text" placeholder="Enter First Name" onChange={(e)=>setFormData({firstName: e.currentTarget.value})}/>
             </Form.Group>
             <Form.Group controlId="formGridFirstName">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter Last Name" />
+              <Form.Control type="text" placeholder="Enter Last Name"  onChange={(e)=>setFormData({lastName: e.currentTarget.value})}/>
             </Form.Group>
             <Form.Group controlId="formGridFirstName">
               <Form.Label>UserName</Form.Label>
-              <Form.Control type="text" placeholder="Enter User Name" />
+              <Form.Control type="text" placeholder="Enter User Name"  onChange={(e)=>setFormData({userName: e.currentTarget.value})}/>
             </Form.Group>
             <Form.Group controlId="formGridFirstName">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter a password" />
-              <Form.Text className="text-muted">
-                Password must be at least 8 characters long.
+              <Form.Control type="password" placeholder="Enter a password"  onChange={(e)=>handlePasswordInput(e.currentTarget.value)}/>
+              <Form.Text className={passwordHelpText.style}>
+                {passwordHelpText.text}
               </Form.Text>
             </Form.Group>
           </Col>
@@ -50,42 +116,44 @@ export default function RegisterForm() {
             <Form.Group controlId="FormUserRole">
               <Form.Label>User Role</Form.Label>
               <Form.Control
-                disabled={disableRoles.preRoles}
                 as="select"
                 defaultValue="Choose Role..."
-                onChange={(e) =>
-                  e.currentTarget.value == 0
-                    ? setDisableRoles({ customRoles: false })
-                    : setDisableRoles({ customRoles: true })
-                }
+                onChange={e=>handleFormRoleSelection(e)}
               >
-                <option value="0">Choose Role...</option>
-                <option value="1">Administrator</option>
-                <option value="2">Personnel</option>
+                <option value="none">Choose Role...</option>
+                <option value="admin">Administrator</option>
+                <option value="personnel">Personnel</option>
               </Form.Control>
             </Form.Group>
             <Form.Group>
               <Form.Text className="lead my-1">Custom Role</Form.Text>
               <Form.Check
+                disable={enableCustomRole}
                 className="my-3"
                 type="checkbox"
                 label="Enable Custom Roles"
-                onChange={(e) => handleCustomeRole(e)}
+                onChange={(e) => handleCustomRoleEnabler(e)}
               />
               <Form.Check
-                disabled={disableRoles.customRoles}
+                disabled={customPermissions.enabled}
                 type="checkbox"
                 label="Access Monitoring Activity"
+                value="monitoring-access"
+                onChange={e=> handleAddPermission(e)}
               />
               <Form.Check
-                disabled={disableRoles.customRoles}
+                disabled={customPermissions.enabled}
                 type="checkbox"
                 label="Access Vehicle Registry"
+                value="vehicle-access"
+                onChange={e=> handleAddPermission(e)}
               />
               <Form.Check
-                disabled={disableRoles.customRoles}
+                disabled={customPermissions.enabled}
                 type="checkbox"
                 label="Access Data Registry"
+                value="data-access"
+                onChange={e=> handleAddPermission(e)}
               />
               <Form.Text className="text-muted my-2">
                 NOTE: Be careful setting roles for a user.
