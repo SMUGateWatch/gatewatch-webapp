e0c4161a622e534a9fd0c90391a66efb4ae7dd5d
John Bryan Delda <mightypirates174@gmail.com>
2020-10-25 20:47:12 +0800

    UI Improvements  -Removed unnecessary actions  -Modal added to confirm action

--- components/permission/manageForm.js
+++ components/permission/manageForm.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Table from "react-bootstrap/Table";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 /**
  * TODO: Map all of the current users
  */
 
+
 export default function ManageForm() {
+
+  
+  var [show, setShow] = useState(false);
+ 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleConfirmDelete = () => {}
   return (
     <React.Fragment>
       <Form className="p-3">
@@ -35,22 +43,45 @@ export default function ManageForm() {
                 <td>pkbr199</td>
                 <td>John Bryan</td>
                 <td>Delda</td>
+                <td>Administrator</td>
                 <td>
-                  <Form.Control as="select" disabled>
-                    <option value="0">Choose Role...</option>
-                    <option value="1">Administrator</option>
-                    <option value="2">Personnel</option>
-                  </Form.Control>
-                </td>
-                <td>
-                  <Button className="mx-1">Edit</Button>
-                  <Button className="mx-1"> Delete</Button>
+                
+                  <Button
+                    
+                    className="mx-1"
+                    variant="danger"
+                    onClick={handleShow}
+                  >
+                    Delete
+                  </Button>
                 </td>
               </tr>
             </tbody>
           </Table>
         </Form.Row>
       </Form>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleConfirmDelete}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </React.Fragment>
   );
 }
