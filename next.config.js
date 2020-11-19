7e4beeb708917d352976a094d92b0acb6e7f62c7
John Bryan Delda <mightypirates174@gmail.com>
2020-11-19 20:21:27 +0800

    fix: fixed config files and package updates

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const path = require('path')
 module.exports={
   
   env:{
-    TITLE: 'Welcome to RPOINT IT SOLUTIONS'
+    TITLE: 'Welcome to RPOINT IT SOLUTIONS',
+    NEXTAUTH_URL: 'http://localhost:3000/pages/api/auth'
   },
   sassOptions:{
       includePaths:[path.join(__dirname,'styles')]
